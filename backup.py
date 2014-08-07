4b02b14c87f60d7b4d2a3f58e50c30e34dea2446
Nate <catalyst@catalyst.pw>
2014-08-06 20:17:40 -0700

    Update backup.py
    Added ability to follow symlinks, simplified things with
    shutil.copytree, and added some drive retry functionality

--- backup.py
+++ backup.py
@@ -27,7 +27,7 @@ def check_config(data):
 
 	''' check for validity of config file '''
 
-	missing = filter(lambda x: not x in data, ["Drive", "Dirs", "Interval", "Zip"])
+	missing = filter(lambda x: not x in data, ["Drive", "Dirs", "Interval", "Zip", "FollowSym", "RetryDrive", "RetryIntv", "RetryTimes"])
 	if missing:
 		print "Error: Field%s: %s missing from config.json" % ("s"*(len(missing) < 1), missing[0] if len(missing) == 1 else str(missing))
 		sys.exit(-1)
@@ -62,6 +62,31 @@ def check_config(data):
 		print "Error: Zip must be a boolean"
 		sys.exit(-1)
 
+	val = data["FollowSym"]
+	if not isinstance(val, bool):
+		print "Error: FollowSym must be a boolean"
+		sys.exit(-1)
+
+	val = data["RetryDrive"]
+	if not isinstance(val, bool):
+		print "Error: RetryDrive must be a boolean"
+		sys.exit(-1)
+	if val:
+		val = data["RetryIntv"]
+		if not isinstance(val, int):
+			print "Error: RetryIntv must be an integer"
+			sys.exit(-1)
+		if not val > 0:
+			print "Error: Interval must be > 0"
+			sys.exit(-1)
+
+		val = data["RetryTimes"]
+		if not isinstance(val, int):
+			print "Error: RetryTimes must be an integer"
+			sys.exit(-1)
+		if not val > 0:
+			print "Error: RetryTimes must be > 0"
+
 def load_config():
 	global config
 	
@@ -87,7 +112,9 @@ def load_config():
 		print "Error: Couldn't find config.json"
 
 def backup_files():
-	print "Backup started"
+
+	''' backup files '''
+
 	start = time.time()
 
 	dirs = config["Dirs"]
@@ -96,52 +123,52 @@ def backup_files():
 
 	if not os.path.exists("%s:\\" % drv):
 		print "Error: Drive not found"
-		sys.exit(-1)
+		return False
 
+	print "Backup started"
+	
 	path = "%s:\\Backups" % (drv)
 	if not os.path.exists(path):
 		os.mkdir(path)
 
 	dpath = time.strftime("%H.%M.%S %d.%m.%Y", time.localtime())
 	path = os.path.join(path, dpath)
+
 	if zip_:
 		path += ".zip"
 		zfile = zipfile.ZipFile(path, "w")
-	else:
-		if not os.path.exists(path):
-			os.mkdir(path)
 
 	for dir_ in dirs:
-		for p, dnames, fnames in os.walk(dir_):
+		if zip_:
+			for path, dirs, files in os.walk(dir_):
+				for f in files:
+					zfile.write(os.path.join(path, f))
+			zfile.close()
+		else:
+			try:
+				shutil.copytree(dir_, path, symlinks=config["FollowSym"])
+			except:
+				print "Error: Failed to copy files"
+				sys.exit(-1)
 
-			op = p
-			p = p.split("\\")
-			p[0] = p[0].replace(":", "")
-			ps = ["\\".join(p[:i]) for i in range(1,len(p)+1)]
-			if not zip_:
-				for d_path in ps:
-					jp = os.path.join(path, d_path)
-					if not os.path.exists(jp):
-						os.mkdir(jp)
-
-			p = os.path.join(path, ps[-1])
-			for file_ in fnames:
-				if zip_:
-					zfile.write(os.path.join(op, file_), os.path.join(op, file_))
-				else:
-					try:
-						shutil.copy(os.path.join(op, file_), p)
-					except:
-						print "Error: Couldn't copy file %s" % os.path.join(op, file_)
-	if zip_:
-		zfile.close()
 	print "Backup completed in %d seconds" % int(time.time() - start)
+	return True
 
 def main():
 	load_config()
+	retries = 0
 	while True:
-		backup_files()
-		time.sleep(config["Interval"])
+		_ = backup_files()
+		if _:
+			time.sleep(config["Interval"])
+		else:
+			retries += 1
+			if retries <= config["RetryTimes"]:
+				print "Retrying in %d seconds ..." % config["RetryIntv"]
+				time.sleep(config["RetryIntv"])
+			else:
+				print "RetryTimes exhausted. Giving up."
+				break
 
 if __name__ == "__main__":
 	main()
